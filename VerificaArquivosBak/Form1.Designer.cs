ce95fad607a3cb0f72134a3c9654c06898ec2e21
NAPO-PC <Administrador@NAPO-PC>
2022-03-17 15:17:30 -0300

    Adicionar arquivos de projeto.

---
+++ VerificaArquivosBak/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace VerificaArquivosBak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiltroTamanhoArquivo = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Location = new System.Drawing.Point(149, 16);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcurar.TabIndex = 0;
+            this.btnProcurar.Text = "Buscar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(652, 226);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(230, 16);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtFiltroTamanhoArquivo);
+            this.groupBox1.Location = new System.Drawing.Point(0, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 45);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kb";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tamanho mínimo";
+            // 
+            // txtFiltroTamanhoArquivo
+            // 
+            this.txtFiltroTamanhoArquivo.Location = new System.Drawing.Point(101, 19);
+            this.txtFiltroTamanhoArquivo.Name = "txtFiltroTamanhoArquivo";
+            this.txtFiltroTamanhoArquivo.Size = new System.Drawing.Size(58, 20);
+            this.txtFiltroTamanhoArquivo.TabIndex = 0;
+            this.txtFiltroTamanhoArquivo.Text = "30000";
+            this.txtFiltroTamanhoArquivo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnProcurar);
+            this.groupBox2.Controls.Add(this.btnExcluir);
+            this.groupBox2.Location = new System.Drawing.Point(337, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(315, 45);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controles";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 287);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(654, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(639, 17);
+            this.toolStripStatusLabel2.Spring = true;
+            this.toolStripStatusLabel2.Text = "0 Kb encontrados";
+            this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 309);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Encontra BAKs:";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFiltroTamanhoArquivo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ VerificaArquivosBak/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VerificaArquivosBak
+{
+    
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Width = this.Width - 10;
+            dataGridView1.Height = statusStrip1.Location.Y - dataGridView1.Location.Y - 5;
+            dataGridView1.ScrollBars = ScrollBars.Both;
+        }
+
+        private void ExibirArquivos()
+        {
+            String procurarEm = @"F:\Backup_Clientes";
+            List<FileInfo> listaArqs = new List<FileInfo>();
+            int filtraTamanhoMinimo = 30000; //em Kb
+            string filtraExtensao = "*.BAK";
+
+            try
+            {
+                filtraTamanhoMinimo = Int32.Parse(txtFiltroTamanhoArquivo.Text);
+            }
+            catch (FormatException  fex)
+            {
+                txtFiltroTamanhoArquivo.Text = filtraTamanhoMinimo.ToString();
+            }
+
+
+            toolStripStatusLabel1.Text = "Buscando arquivos...";
+            this.Update();
+
+            Directory.GetDirectories(procurarEm, "*", SearchOption.AllDirectories).ToList<String>().ForEach(
+                nomeDir => Directory.GetFiles(nomeDir, filtraExtensao, SearchOption.AllDirectories).ToList<String>().ForEach(
+                    nomeArquivo => listaArqs.Add(new FileInfo(nomeArquivo))
+                )
+            );
+
+            var lista = from x in listaArqs
+                        where (x.Length / 1024) > filtraTamanhoMinimo
+                        select new
+                        {
+                            Diretório = x.DirectoryName,
+                            Arquivo = x.Name,
+                            Tamanho_Kb = String.Format("{0:0,0}", (x.Length / 1024 ))
+                        };
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.DataSource = lista.ToArray();
+            dataGridView1.AutoResizeColumns();
+
+            toolStripStatusLabel2.Text =  String.Format("{0:0,0} Kb Encontrados", listaArqs.Sum(x => (x.Length/1024)) );
+            toolStripStatusLabel1.Text = "Busca concluida!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExibirArquivos();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            String nomeArquivo = "";
+
+            DialogResult dialog = MessageBox.Show("Deseja mesmo excluir arquivo(s) selecionado(s)?", "Excluir", MessageBoxButtons.YesNo);
+            if( dialog != DialogResult.Yes ) { return; }
+
+
+            try
+            {
+                foreach (DataGridViewRow xRow in dataGridView1.SelectedRows)
+                {
+                    nomeArquivo = xRow.Cells["Diretório"].Value.ToString()
+                                  + "\\"
+                                  + xRow.Cells["Arquivo"].Value.ToString();
+
+                    if(File.Exists(nomeArquivo))
+                    {
+                        File.Delete(nomeArquivo);
+                    }
+                }
+
+                ExibirArquivos();
+                MessageBox.Show("Itens excluidos!", "Excluir");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "Excluir");
+            }
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            dataGridView1.Width = this.Width - 10;
+            dataGridView1.Height = statusStrip1.Location.Y - dataGridView1.Location.Y - 5 ;
+        }
+    }
+
+
+    
+}
